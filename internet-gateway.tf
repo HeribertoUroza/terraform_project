cf91a221fbd1006c640a9dffa07132e016c536e5
heriberto <heriberto.uroza813@gmail.com>
2022-10-17 03:31:25 -0400

    create internet gateway for terra_vpc

---
+++ internet-gateway.tf
@@ -0,0 +1,10 @@
+# Resource aws_internet_gateway
+# Link: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  tags = {
+    Name = "Terraform IGW"
+  }
+}
